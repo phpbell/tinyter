73a37b4c9cd6776c240141bef7d18870b16aff9c
Anderson Ismael Couto da Silva <73040867+andericsilva@users.noreply.github.com>
2020-12-23 15:13:31 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$cfg=require 'cfg.php';
+?>
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title><?php print $cfg['site_name']; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
