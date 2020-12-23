1d6664a4ce0c735d1860df11aa93cf9cf6ff1d54
Anderson Ismael Couto da Silva <73040867+andericsilva@users.noreply.github.com>
2020-12-23 15:22:16 -0300

    bs2 adicionado

--- index.php
+++ index.php
@@ -2,13 +2,36 @@
 $cfg=require 'cfg.php';
 ?>
 <!DOCTYPE html>
-<html lang="pt" dir="ltr">
+<html lang="<?php print $cfg['site_language']; ?>" dir="ltr">
 <head>
     <meta charset="utf-8">
     <title><?php print $cfg['site_name']; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap.min.css" integrity="sha512-dhpxh4AzF050JM736FF+lLVybu28koEYRrSJtTKfA4Z7jKXJNQ5LcxKmHEwruFN2DuOAi9xeKROJ4Z+sttMjqw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css" integrity="sha512-S6hLYzz2hVBjcFOZkAOO+qEkytvbg2k9yZ1oO+zwXNYnQU71syCWhWtIk3UYDvUW2FCIwkzsTcwkEE58EZPnIQ==" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <h1><?php print $cfg['site_name']; ?></h1>
+            </div>
+            <!-- span12 -->
+        </div>
+        <!-- row -->
+        <div class="row">
+            <div class="span12">
+                <ul class="nav nav-tabs nav-stacked">
+                    <li>
+                        <a href="#">Exemplo de link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- container -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/js/bootstrap.min.js" integrity="sha512-28e47INXBDaAH0F91T8tup57lcH+iIqq9Fefp6/p+6cgF7RKnqIMSmZqZKceq7WWo9upYMBLMYyMsFq7zHGlug==" crossorigin="anonymous"></script>
 </body>
 </html>
